c42c3fef4beda370527d94a5a3ef61d3acfb9e73
alexanderpacherrez <pacherrezberru@gmail.com>
2025-07-05 23:38:46 -0500

    C# Prep 3 - Complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 20);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
