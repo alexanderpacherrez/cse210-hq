e7f26ede4f8b68eb5febe69ad8618d039c6b56b5
alexanderpacherrez <pacherrezberru@gmail.com>
2025-07-10 21:30:31 -0500

    Learning Activity 02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2005;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emily Gualan";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
